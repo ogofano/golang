a368ee09e579207f25b6fa43e2b70d895b16f18c
ogofano <mailboxilya65@gmail.com>
2024-11-08 17:50:29 +0300

    add function isLeapYear

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
+func isLeapYear(year int) bool {
+	if year % 400 == 0 {
+		return true
+	} else if year % 4 == 0 && year % 100 != 0 {
+		return true
+	}
+	return false
+}
+
 func main() {
-	
+	var year int
+	fmt.Scan(&year)
+	fmt.Println(isLeapYear(year))
 }
